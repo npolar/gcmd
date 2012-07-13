8ba4e45daf2710dc577fa6eb84f6ae8939a422d5
cnrdh <conrad.helgeland@gmail.com>
2012-07-13 12:52:56 +0200

    Cleaned

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,3 @@ require 'simplecov'
 SimpleCov.start
 
 require "rspec"
-#require "rack/test"
-#
-## Test environment
-#ENV["RACK_ENV"] = "test"
-#
-## https://www.relishapp.com/rspec/rspec-expectations/v/2-8/docs/built-in-matchers/match-matcher
-#RSpec.configure do |conf|
-#  conf.include Rack::Test::Methods
-#end
