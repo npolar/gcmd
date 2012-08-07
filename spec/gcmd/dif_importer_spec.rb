2b34738b925309c44433294461969c10e760728e
RDux <ruben.dens@gmail.com>
2012-08-07 13:16:04 +0200

    added tests for initialization and basic methods

---
+++ spec/gcmd/dif_importer_spec.rb
@@ -0,0 +1,66 @@
+require "spec_helper"
+require "gcmd/dif_importer"
+
+require "nokogiri"
+
+describe Gcmd::DifImporter do
+  
+  subject do
+    Gcmd::DifImporter.new( "spec/data/dif_record.xml" )
+  end
+  
+  context "Importing XML" do
+    
+    it "should have a document if initialized with a XML source" do
+      subject.document.should be_a_kind_of( Nokogiri::XML::Document )
+    end
+    
+    it "shouldn't have a document if initialized without a XML source" do
+      importer = Gcmd::DifImporter.new
+      importer.document.should be( nil )
+    end
+    
+    context "#build_hash_documents" do
+      
+      it "should return an array" do
+        subject.build_hash_documents.should be_a_kind_of( Array )
+      end
+      
+      it "should set document if provided with a XML source" do
+        importer = Gcmd::DifImporter.new
+        importer.build_hash_documents( "spec/data/dif_record.xml" )
+        subject.document.should be_a_kind_of( Nokogiri::XML::Document )
+      end
+      
+      it "should raise an argument error if no document is found" do
+        importer = Gcmd::DifImporter.new
+        expect{ importer.build_hash_documents }.to raise_error( ArgumentError )
+      end
+      
+    end
+    
+    context "#document_to_object" do
+      
+      it "should return an array" do
+        subject.document_to_object.should be_a_kind_of( Array )
+      end
+      
+      it "the result array should consist of Hashes" do
+        subject.document_to_object.each do | doc |
+          doc.should be_a_kind_of( Hash )
+        end
+      end
+      
+    end
+    
+    context "#hash_from_xml" do
+      
+      it "should return a Hash" do
+        subject.hash_from_xml( "" ).should be_a_kind_of( Hash )
+      end
+        
+    end
+    
+  end
+  
+end
